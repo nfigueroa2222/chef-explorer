8b9245cdbd4006b6c065777dab4153a2f31177ab
nfigueroa2222 <NikolasF00@gmail.com>
2024-12-06 12:44:30 -0500

    Home page styling

--- src/components/scripts/PreviewItem.js
+++ src/components/scripts/PreviewItem.js
@@ -1,17 +1,26 @@
-// PreviewItem.js
 import React from "react";
+import PropTypes from 'prop-types';
 import '../css/PreviewItem.css';
 
-const PreviewItem = ({ recipe }) => {
-  const { image, name, servings } = recipe;
+const PreviewItem = ({ id, image, name, servings }) => {
 
-  return (
-    <div className="preview-item">
-      <img src={image} alt={name} className="recipe-image" />
-      <h3 className="recipe-name">{name}</h3>
-      <p className="recipe-servings">Servings: {servings}</p>
-    </div>
-  );
+    return (
+        <div className="preview-item">
+            <div className="recipe-image">
+                <img src={image} alt={name}  />
+            </div>
+            <h3 className="recipe-name">{name}</h3>
+            <p className="recipe-servings">Servings: {servings}</p>
+        </div>
+    );
+};
+
+// Use PropTypes for type-checking
+PreviewItem.propTypes = {
+    id: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    servings: PropTypes.number.isRequired
 };
 
 export default PreviewItem;
--- src/components/scripts/RecipeGallery.js
+++ src/components/scripts/RecipeGallery.js
@@ -7,34 +7,39 @@ const RecipeGallery = () => {
     const carouselRef = useRef(null); /* Creates a reference to the carousel element to manipulate it aka scroll */
 
     const scrollLeft = () => {
-      carouselRef.current.scrollBy({
-        left: -300, // Adjust based on the card width
-        behavior: "smooth",
-      });
-    };
-  
-    const scrollRight = () => {
-      carouselRef.current.scrollBy({
-        left: 300, // Adjust based on the card width
-        behavior: "smooth",
-      });
+        carouselRef.current.scrollBy({
+            left: -260, // Adjust based on the card width and padding
+        });
     };
 
-  return (
-    <div className="recipe-gallery">
-      <button className="carousel-button left" onClick={scrollLeft}>
-        ❮
-      </button>
-      <div className="carousel" ref={carouselRef}>
-        {recipes.map((recipe, index) => (
-          <PreviewItem key={index} recipe={recipe} />
-        ))}
-      </div>
-      <button className="carousel-button right" onClick={scrollRight}>
-        ❯
-      </button>
-    </div>
-  );
+    const scrollRight = () => {
+        carouselRef.current.scrollBy({
+            left: 260, // Adjust based on the card width and padding
+        });
+    };
+
+    return (
+        <div className="recipe-gallery">
+            <button className="carousel-button left" onClick={scrollLeft}>
+                &#9668; { /* unicode for left arrow */ }
+            </button>
+            <div className="carousel" ref={carouselRef}>
+                { /* use the recipes.json file to populate */ }
+                {recipes.map((previewItem) => (
+                    <PreviewItem
+                        key={previewItem.id}
+                        id={previewItem.id}
+                        image={previewItem.image}
+                        name={previewItem.name}
+                        servings={previewItem.servings}
+                    />
+                ))}
+            </div>
+            <button className="carousel-button right" onClick={scrollRight}>
+                &#9658; { /* unicode for right arrow */ }
+            </button>
+        </div>
+    );
 };
 
 export default RecipeGallery;
